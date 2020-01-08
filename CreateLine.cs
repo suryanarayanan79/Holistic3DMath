cd1dce754ced53661586c7714111fe88ab72779a
suryanarayanan <suryanarayanan.rs@gmail.com>
2020-01-08 07:52:03 +0530

    Sphere Move Scene
    Sphere Move Scene Added

--- CreateLine.cs
+++ CreateLine.cs
@@ -10,10 +10,10 @@ public class CreateLine : MonoBehaviour
     void Start()
     {
         L1 = new Line(new Coords(-100,0,0),new Coords(200,150,0));
-        L1.DrawLine(1,Color.red);
+        L1.Draw(1,Color.red);
         L2 = new Line(new Coords(0,-100,0),new Coords(0,200,0));
-        L2.DrawLine(1,Color.white);
-        float timeOfIntersection = L1.IntersectAt(L2);
+        L2.Draw(1,Color.white);
+        float timeOfIntersection = L1.IntersectsAt(L2);
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         sphere.transform.position = L1.Lerp(timeOfIntersection).ToVector();
     }
